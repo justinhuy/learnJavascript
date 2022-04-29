afe1a27ae80c39e4be6724eafc0d6b7330f9278d
JustinHuy <trannhathuy1305@gmail.com>
2022-04-29 10:38:14 +0700

    j

--- Part 1/Coding Chalenge/challenge4.js
+++ Part 1/Coding Chalenge/challenge4.js
@@ -1,3 +1,5 @@
 
 let bill = 275;
-console.log(`The bill was ${bill}, the tip was ${const tip = (50 < bill < 300 ? bill * 0.15 : bill * 0.2, and the total value ${tip + bill}`);
+let tip = (50 < bill <= 300) ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the toal value ${bill + tip}`);
